4032579e15a17d9db113d187bef8ad69b3383ba5
Rainer Ramos <rainermr89@hotmail.com>
2021-09-16 09:00:39 -0300

    Utilizando estrutura de repetição (for e while)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,37 @@
-fun main () {
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Rainer"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 1000
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Rainer $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
 
-    if(saldo > 0.0){
+
+//    for (i in 5 downTo 1) {
+//
+//        if(i == 4) {
+//            continue
+//        }
+
+
+//    testaCondicoes(saldo)
+
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
         println("conta é positiva")
-    } else if (saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("conta é neutra")
     } else {
-       println("conta é negativa")
+        println("conta é negativa")
     }
 
 
