6c0d09725dc6e48ccd9c7b461462356825d6f899
Rainer Ramos <rainermr89@hotmail.com>
2021-09-16 10:06:51 -0300

    Cópia e referência

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,13 +10,32 @@ fun main() {
     contaVanessa.numero = 1001
     contaVanessa.saldo = 300.0
 
-    println(contaVanessa.titular)
-    println(contaVanessa.numero)
-    println(contaVanessa.saldo)
+//    println(contaVanessa.titular)
+//    println(contaVanessa.numero)
+//    println(contaVanessa.saldo)
+//
+//    println(contaRainer.titular)
+//    println(contaRainer.numero)
+//    println(contaRainer.saldo)
 
-    println(contaRainer.titular)
-    println(contaRainer.numero)
-    println(contaRainer.saldo)
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
 }
 
 class Conta {
