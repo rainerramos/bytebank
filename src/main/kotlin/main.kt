f318f6ea8a38b4d702318d14b00f5585d99151f7
Rainer Ramos <rainermr89@hotmail.com>
2021-09-22 10:28:45 -0300

    Criando uma nova classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,15 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val rainer = Funcionario(
+        nome = "Rainer",
+        cpf = "222222222-22",
+        salario = 1000.0
+    )
+
+    println("nome ${rainer.nome}")
+    println("cpf ${rainer.cpf}")
+    println("salario ${rainer.salario}")
+    println("bonificação ${rainer.bonificacao()}")
 }
 
