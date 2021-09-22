d248df7f3baabb8bb38ac7ccf503dc06991ac3fd
Rainer Ramos <rainermr89@hotmail.com>
2021-09-22 14:00:00 -0300

    Criando classes para cada funcionário

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,28 +1,10 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double,
-    val tipo: Int //0 funcionario, 1 gerente, 2 diretor
+    val salario: Double
 ) {
-    fun bonificacao(): Double{
-        when (tipo) {
-            0 -> {
-                return salario * 0.1
-            }
-            1 -> {
-                return salario * 0.2
-            }
-            else -> {
-                return salario * 0.3
-            }
-        }
+
+    fun bonificacao(): Double {
         return salario * 0.1
     }
-
-    fun autentica(senha: Int){
-        if (tipo == 1) {
-            // pode autenticar
-        }
-    }
-
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,22 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val rainer = Funcionario(
-        nome = "Rainer",
-        cpf = "222222222-22",
-        salario = 1000.0,
-        tipo = 0
+    val vanessa = Gerente(
+        nome = "Vanessa",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
     )
 
-    println("nome ${rainer.nome}")
-    println("cpf ${rainer.cpf}")
-    println("salario ${rainer.salario}")
-    println("bonificação ${rainer.bonificacao()}")
+    println("nome ${vanessa.nome}")
+    println("cpf ${vanessa.cpf}")
+    println("salario ${vanessa.salario}")
+    println("bonificação ${vanessa.bonificacao()}")
+
+    if (vanessa.autentica(1234)){
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
 }
 
