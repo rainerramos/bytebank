0d26df68062ecbb29cd59ec88cd07530d41957c2
Rainer Ramos <rainermr89@hotmail.com>
2021-10-09 16:46:35 -0300

    Conhecendo o Object Expressions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -5,6 +5,18 @@ import br.com.alura.bytebank.modelo.totalContas
 
 
 fun main() {
+
+   val vanessa = object {
+      val nome: String = "Vanessa"
+      val cpf: String = "111.111.111-11"
+      val senha: Int = 1000
+
+      fun autentica(senha: Int) = this.senha == senha
+   }
+
+
+   println("nome do cliente ${vanessa.nome}")
+
    val rainer = Cliente(nome = "Rainer", cpf = "", senha = 1)
    val contaPoupanca = ContaPoupanca(titular = rainer, numero = 1000)
    val contaCorrente = ContaCorrente(titular = rainer, numero = 1001)
